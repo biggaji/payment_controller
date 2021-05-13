2bf6874fe926fbd6d363611df8ef0030d193dcfe
biggaji <tobiajibade2017@yandex.com>
2021-05-12 19:25:27 -0700

    added logic to not allow user to send money to itself

--- src/controller/payment.js
+++ src/controller/payment.js
@@ -9,12 +9,24 @@ class Payment {
         if (amount > this.balance) {
             return `Insufficent fund, please load your account with money!`;
         }
-        else {
-            this.balance = this.balance - amount;
-            return `${this.from} sent $${amount} to ${to} on ${new Date().toDateString()}`;
+        if (this.from === to) {
+            return `You can't send money to yourself`;
         }
+        this.balance = this.balance - amount;
+        return `${this.from} sent $${amount} to ${to} on ${new Date().toDateString()}`;
     }
     viewBalance() {
         return this.balance;
+    }
+}
+// A shop payment to inherit features from payment class
+class ShopPay extends Payment {
+    constructor(from, shopName) {
+        super(from);
+        this.shopName = shopName;
+        this.balance = 200000;
+    }
+    get shopname() {
+        return this.shopName;
     }
 }
